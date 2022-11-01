41452ce280244f30f264b6613e7d2144db2302ea
wuyefan <313633306@qq.com>
2022-11-01 21:43:56 +0800

    初始化脚手架

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Hello from "./components/Hello";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import hello from "./index.module.css";
+
+export default class Hello extends Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
